710ee5cdc510130b3e9264f2e8d95f6123595152
riccione <riccione@protonmail.com>
2022-11-26 23:16:02 -0500

    Create a stub for process.py, add gitignore

---
+++ process_words/process.py
@@ -0,0 +1,17 @@
+'''
+Goal:
+Get new (unknown) words from the text or link
+
+Process txt file to get a word list from it
+If the word is in known list => exclude it from the final list
+Create a final result list with new words
+'''
+
+def read_file():
+    pass
+
+def is_known():
+    pass
+
+def save_file():
+    pass
