c234b0396909b8752bf5ae2522ea6aa72e3e98aa
riccione <riccione@protonmail.com>
2022-11-27 22:19:06 -0500

    Add a basic logic to process.py Updated .gitignore
    process.py contains 4 different fn:
    - read_file
    - clean
    - is_known
    - save_file
    The names of fn are self explanatory

--- process_words/process.py
+++ process_words/process.py
@@ -6,12 +6,40 @@ Process txt file to get a word list from it
 If the word is in known list => exclude it from the final list
 Create a final result list with new words
 '''
+import re
 
-def read_file():
-    pass
 
-def is_known():
-    pass
+def read_file(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+        data = clean(data)
+        words = set(data.split(' '))
+    return list(words)
 
-def save_file():
-    pass
+def clean(x):
+    x = re.sub('[\'\"”\.,;:!?+\-=!@#$%^&*—/]', '', x)
+    x = re.sub('[\(\)]', '', x)
+    x = re.sub('\d', '', x)
+    x = x.replace('\n', '')
+    return x.lower()
+
+def is_known(x):
+    xz = []
+    with open('./known.txt', 'r') as f:
+        data = f.read()
+        for w in x:
+            if w not in data and w != '':
+                xz.append(w)
+    return xz
+
+
+def save_file(x):
+    with open('./final.txt', 'w') as f:
+        for v in x:
+            f.write(f"{v}\n")
+
+
+data = read_file('./sample.txt')
+data = is_known(data)
+data = sorted(data)
+save_file(data)
