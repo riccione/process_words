2e04752f0018ae83cc0218c32a17ec06d91783ab
riccione <riccione@protonmail.com>
2022-12-04 22:38:09 -0500

    Add frequency of the words Update .gitignore Add known.txt

--- process_words/process.py
+++ process_words/process.py
@@ -6,7 +6,7 @@ Process txt file to get a word list from it
 If the word is in known list => exclude it from the final list
 Create a final result list with new words
 
-TODO: measure performance
+TODO: measure performance using profiler
 TODO: argparse (filename for read)
 TODO: make a class
 TODO: make a library
@@ -24,16 +24,28 @@ def read_file(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
         words = make_word_list(lines)
-    return list(words)
+    return words
 
 
 def make_word_list(xz):
     words = []
-    for x in xz:
-        xy = x.split()
-        for v in xy:
-            words.append(clean(v))
-    return set(words)
+    with open('./known.txt', 'r') as f:
+        known = f.read()
+        for x in xz:
+            xy = x.split()
+            for v in xy:
+                v = clean(v)
+                if v not in known and v[:-1] not in known and v != '':
+                    words.append(v)
+    return words
+
+
+def count_frequency(xz, xz_set):
+    xy = []
+    for x in xz_set:
+        if x != '':
+            xy.append((x, xz.count(x)))
+    return xy
 
 
 '''
@@ -42,22 +54,7 @@ remove punctuation, new lines
 
 
 def clean(x):
-    return re.sub(r'[^a-zA-Z]', '', x).lower()
-
-
-'''
-known.txt file contains the list of known words
-'''
-
-
-def is_known(x):
-    xz = []
-    with open('./known.txt', 'r') as f:
-        data = f.read()
-        for w in x:
-            if w not in data and w != '':
-                xz.append(w)
-    return xz
+    return re.sub(r'[^a-zA-Z\']', '', x).lower()
 
 
 '''
@@ -69,12 +66,16 @@ def save_file(x):
     with open('./final.txt', 'w') as f:
         last = 0
         for i, v in enumerate(x):
-            f.write(f"{v}\n")
+            f.write(f"{v[0]}, {v[1]}\n")
             last = i
     print(f"Unknown words: {last}")
 
 
+def sort_by_frequency(xz):
+    return sorted(xz, key=lambda x: x[1], reverse=True)
+
+
 data = read_file('./sample.txt')
-data = is_known(data)
-data = sorted(data)
+data = count_frequency(data, set(data))
+data = sort_by_frequency(data)
 save_file(data)
